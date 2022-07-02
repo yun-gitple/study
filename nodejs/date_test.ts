30a9b8c4c385ccc3eaf99487bd990e0c6862b92e
jongha.yun <jongha.yun@gitplecorp.com>
2022-07-02 11:54:31 +0900

    test date

--- nodejs/date_test.ts
+++ nodejs/date_test.ts
@@ -1,7 +1,9 @@
 var date0 = new Date('2022/07/01');       //KST로 인식
-var date1 = new Date('2022/07/01/00:30'); //KST로 인식
+var date1 = new Date('2022/07/01T00:30'); //KST로 인식
+var date1 = new Date('2022/07/01 00:30'); //KST로 인식
 var date2 = new Date('2022-07-01');       //UTC로 인식
-var date3 = new Date('2022-07-01/00:30'); //KST로 인식
+var date3 = new Date('2022-07-01T00:30'); //KST로 인식
+var date3 = new Date('2022-07-01 00:30'); //KST로 인식
 
 console.log('date0: ', date0, ", zone: ", new Date(date0.toLocaleString("en-US", {timeZone: "Asia/Seoul"})));
 console.log('date1: ', date1, ", zone: ", new Date(date1.toLocaleString("en-US", {timeZone: "Asia/Seoul"})));
