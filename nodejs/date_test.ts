fc6c925ec11cc93a652597d6a9ebefa5eefc804a
jongha.yun <jongha.yun@gitplecorp.com>
2022-07-08 20:12:14 +0900

    add new tests

--- nodejs/date_test.ts
+++ nodejs/date_test.ts
@@ -1,11 +1,30 @@
-var date0 = new Date('2022/07/01');       //KST로 인식
-var date1 = new Date('2022/07/01T00:30'); //KST로 인식
-var date1 = new Date('2022/07/01 00:30'); //KST로 인식
-var date2 = new Date('2022-07-01');       //UTC로 인식
-var date3 = new Date('2022-07-01T00:30'); //KST로 인식
-var date3 = new Date('2022-07-01 00:30'); //KST로 인식
+function a() {
+  const date0 = new Date('2022/07/01');       //KST로 인식
+  const date1 = new Date('2022/07/01T00:30'); //KST로 인식
+  const date11 = new Date('2022/07/01 00:30'); //KST로 인식
+  const date2 = new Date('2022-07-01');       //UTC로 인식
+  const date3 = new Date('2022-07-01T00:30'); //KST로 인식
+  const date33 = new Date('2022-07-01 00:30'); //KST로 인식
 
-console.log('date0: ', date0, ", zone: ", new Date(date0.toLocaleString("en-US", {timeZone: "Asia/Seoul"})));
-console.log('date1: ', date1, ", zone: ", new Date(date1.toLocaleString("en-US", {timeZone: "Asia/Seoul"})));
-console.log('date2: ', date2, ", zone: ", new Date(date2.toLocaleString("en-US", {timeZone: "Asia/Seoul"})));
-console.log('date3: ', date3, ", zone: ", new Date(date3.toLocaleString("en-US", {timeZone: "Asia/Seoul"})));
+  console.log('date0: ', date0, ", zone: ", new Date(date0.toLocaleString("en-US", {timeZone: "Asia/Seoul"})));
+  console.log('date1: ', date1, ", zone: ", new Date(date1.toLocaleString("en-US", {timeZone: "Asia/Seoul"})));
+  console.log('date11: ', date11, ", zone: ", new Date(date11.toLocaleString("en-US", {timeZone: "Asia/Seoul"})));
+  console.log('date2: ', date2, ", zone: ", new Date(date2.toLocaleString("en-US", {timeZone: "Asia/Seoul"})));
+  console.log('date3: ', date3, ", zone: ", new Date(date3.toLocaleString("en-US", {timeZone: "Asia/Seoul"})));
+  console.log('date33: ', date33, ", zone: ", new Date(date33.toLocaleString("en-US", {timeZone: "Asia/Seoul"})));
+}
+
+function b() {
+
+  const date0 = new Date('2022/06/01');       //KST로 인식
+  const date00 = new Date('20220601');       //KST로 인식
+  const date1 = new Date('2022-06-29T06:54:27.000Z'); //KST로 인식
+
+  console.log('date0: ', date0, ', getTime: ', date0.getTime());
+  console.log('date00: ', date00, ', getTime: ', date00.getTime());
+  console.log('date1: ', date1, ', getTime: ', date1.getTime());
+}
+
+a();
+console.log('~~~~~~~~~~~~~');
+b();
