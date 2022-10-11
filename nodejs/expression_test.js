69e20aff726f1ebf4ab15a7c36ef2d689ae45852
jongha.yun <jongha.yun@gitplecorp.com>
2022-10-11 14:32:32 +0900

    fix error

--- nodejs/expression_test.js
+++ nodejs/expression_test.js
@@ -1,12 +1,25 @@
-var data = 'Oh';
+// a();
+function a() {
+  var data = 'Oh';
 
-console.log(data? 'OK': 'NO');
+  console.log(data? 'OK': 'NO');
 
-var cond = 'development';
+  var cond = 'development';
 
-console.log(cond, 'log level: ', 
-cond === 'local' || !cond
-              ? 'debug'
-              : cond === 'development'
-              ? 'info'
-              : 'warn');
+  console.log(cond, 'log level: ', 
+  cond === 'local' || !cond
+                ? 'debug'
+                : cond === 'development'
+                ? 'info'
+                : 'warn');
+}
+
+b();
+function b() {
+  let a = 0;
+  let b = 10;
+
+  a = b = 108;
+
+  console.log('data:', a, b);
+}
